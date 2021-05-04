364f5de85944933c15abe56168f4ad6b249bbbad
StevenLiem <Stevenliem377@gmail.com>
2021-05-04 16:29:12 +0800

    Add project files.

---
+++ HomeWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PCS_Gaming
+{
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        OracleConnection conn;
+        public HomeWindow(OracleConnection conn)
+        {
+            InitializeComponent();
+
+            this.conn = conn;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PCS_Gaming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string dataSource, user, pass;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void minimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void maximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            dataSource = TBDS.Text;
+            user = TBUser.Text;
+            pass = TBPass.Password;
+            var connectionString = "Data Source = " + dataSource + "; User ID=" + user + "; Password=" + pass;
+            OracleConnection conn = new OracleConnection(connectionString);
+            try
+            {
+                conn.Open();
+                conn.Close();
+                HomeWindow home = new HomeWindow(conn);
+                home.ShowDialog();
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+            }
+        }
+    }
+}
